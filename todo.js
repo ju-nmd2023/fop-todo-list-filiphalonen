316896337c8c03206924d578b976b0dfaab51cb8
filiphalonen <157317694+filiphalonen@users.noreply.github.com>
2024-04-16 18:33:15 +0200

    add item to local storage

--- todo.js
+++ todo.js
@@ -1,3 +1,10 @@
-function addTask() {
-  alert("test");
+function submitTask() {
+  //   const form = document.querySelector("form[name='todo-form']");
+  const taskName = document.querySelector("input[name='task-name']").value;
+  console.log(taskName);
+  addToLocalStorage(taskName);
+}
+
+function addToLocalStorage(taskName) {
+  localStorage.setItem(taskName, taskName);
 }
