c7591b42dc147c0682d24f9d12f313f720463d14
filiphalonen <157317694+filiphalonen@users.noreply.github.com>
2024-04-16 19:07:02 +0200

    get tasks from local storage

--- todo.js
+++ todo.js
@@ -1,10 +1,31 @@
 function submitTask() {
   //   const form = document.querySelector("form[name='todo-form']");
   const taskName = document.querySelector("input[name='task-name']").value;
-  console.log(taskName);
   addToLocalStorage(taskName);
 }
 
 function addToLocalStorage(taskName) {
-  localStorage.setItem(taskName, taskName);
+  if (localStorage.getItem("taskList") == undefined) {
+    localStorage.setItem("taskList", JSON.stringify([taskName]));
+  } else {
+    let list = JSON.parse(localStorage.getItem("taskList"));
+    list.push(taskName);
+    localStorage.setItem("taskList", JSON.stringify(list));
+  }
+}
+
+function getFromLocalStorage() {
+  let list = JSON.parse(localStorage.getItem("taskList"));
+  list.forEach((element) => {
+    displayTask(element);
+  });
+}
+
+getFromLocalStorage();
+
+function displayTask(element) {
+  let taskCard = document.createElement("div");
+  let taskTitle = document.createTextNode(element);
+  taskCard.appendChild(taskTitle);
+  document.querySelector("#task-list").appendChild(taskCard);
 }
